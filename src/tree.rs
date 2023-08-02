75aa46b3d5d5357fa6d8a3c715368907db6d0095
ConnorArmstrong <connor.armstrong@pm.me>
2023-08-02 22:03:14 +1000

    more work

--- src/tree.rs
+++ src/tree.rs
@@ -1,3 +1,25 @@
 
 use std::rc::Rc;
 use std::cell::RefCell;
+
+use crate::Move;
+
+pub struct MoveTree {
+    turn_number: usize,
+    turn: Move,
+    children: Vec<Rc<RefCell<MoveTree>>>,
+}
+
+impl MoveTree {
+    pub fn new(turn: Move) -> Self {
+        MoveTree {
+            turn_number: turn.move_number,
+            turn,
+            children: Vec::new(),
+        }
+    }
+
+    pub fn add_child(&mut self, child: Rc<RefCell<MoveTree>>) {
+        self.children.push(child);
+    }
+}
--- target/debug/deps/go_rs.d
+++ target/debug/deps/go_rs.d
@@ -1,6 +1,7 @@
-C:\Users\Connor\Documents\go-rs\target\debug\deps\go_rs.exe: src\main.rs src\group.rs
+C:\Users\Connor\Documents\go-rs\target\debug\deps\go_rs.exe: src\main.rs src\group.rs src\tree.rs
 
-C:\Users\Connor\Documents\go-rs\target\debug\deps\go_rs.d: src\main.rs src\group.rs
+C:\Users\Connor\Documents\go-rs\target\debug\deps\go_rs.d: src\main.rs src\group.rs src\tree.rs
 
 src\main.rs:
 src\group.rs:
+src\tree.rs:
